3cbf8b9c807a88ee092e1c68fc87ad07f283b7c6
Nicholas Moreles <nicholas.moreles@utexas.edu>
2014-08-19 18:36:05 -0500

    Update config
    Details:
    - Update config to automatically utilize OS and architecture specific BLAS (once included in future).

--- blaspy/config.py
+++ blaspy/config.py
@@ -9,22 +9,36 @@
 
 """
 
+from ctypes import cdll
+from os import path
+from platform import system
+from struct import calcsize
 
-import ctypes as c
-import os
+# The name of the BLAS .so or .dll file. By default this is the OpenBLAS reference
+# implementation bundled with BLASpy. Only modify if you wish to use a different version of BLAS.
+BLAS_NAME_OVERRIDE = ""  # default is ""
 
-# The name of the BLAS .so file. By default this is the OpenBLAS reference implementation included
-# with BLASpy. Only modify if you wish to use a different version of BLAS you have installed.
-BLAS_NAME = "libopenblasp-r0.2.9-64ref32threads.so"
-
-# True if the BLAS .so file is in the blaspy/lib subdirectory,
+# True if the BLAS .so or .dll file is in the blaspy/lib subdirectory,
 # False if Python should search for it in /usr/lib
-IN_BLASPY_SUBDIR = True
+IN_BLASPY_SUBDIR = True  # default is True
 
 ##################################
 # DO NOT EDIT BELOW THIS COMMENT #
 ##################################
 
+# find the appropriate BLAS to use
+if BLAS_NAME_OVERRIDE == "":
+    if system() == "Windows":
+        if calcsize("P") == 8:  # 64-bit
+            BLAS_NAME = "NOT_IMPLEMENTED.dll"
+        else:  # 32-bit
+            BLAS_NAME = "NOT_IMPLEMENTED.dll"
+    else:  # Linux
+        if calcsize("P") == 8:  # 64-bit
+            BLAS_NAME = "libopenblasp-r0.2.9-64ref32threads.so"
+        else:  # 32-bit
+            BLAS_NAME = "NOT_IMPLEMENTED.dll"
+
 # Create the appropriate path to _libblas
-BLAS_PATH = str(os.path.dirname(__file__))[:-6] + "lib/"
-_libblas = c.cdll.LoadLibrary((BLAS_PATH + BLAS_NAME) if IN_BLASPY_SUBDIR else BLAS_NAME)
+BLAS_PATH = str(path.dirname(__file__))[:-6] + "lib/"
+_libblas = cdll.LoadLibrary((BLAS_PATH + BLAS_NAME) if IN_BLASPY_SUBDIR else BLAS_NAME)
