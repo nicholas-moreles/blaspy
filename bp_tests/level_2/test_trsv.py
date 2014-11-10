fb1fe17883ee089d027bddfc7fce2570463d91ae
nicholas-moreles <nicholas.moreles@utexas.edu>
2014-11-10 13:53:35 -0600

    Update test_trsv.py to avoid numerical issues
    Details:
    - Scale off-diagonal cells by 1/n to prevent numerical issues
    - Set diagonal to values between 1 and 2 to prevent singularity
    - Increase matrix sizes tested as a result
    - Add tests for random strides (fix #2)
    - Correct documentation typo

--- bp_tests/level_2/test_trsv.py
+++ bp_tests/level_2/test_trsv.py
@@ -10,13 +10,13 @@
 """
 
 from ..helpers import random_vector, random_triangular_matrix
-from blaspy import trsv
-from numpy import allclose, copy, dot, fill_diagonal, triu, tril, zeros
+from blaspy import trsv, gemv
+from numpy import allclose, copy, fill_diagonal, zeros
 from itertools import product
-from random import randint
+from random import randint, uniform
 
-N_MIN, N_MAX = 2, 10         # matrix/vector sizes, small to prevent ill-conditioned problems
-#STRIDE_MAX = 5              # max vector stride
+N_MIN, N_MAX = 2, 3000       # matrix/vector sizes
+STRIDE_MAX = 1000            # max vector stride
 RTOL, ATOL = 5e-01, 5e-02    # margin of error
 
 
@@ -33,7 +33,7 @@ def test_trsv():
     # values to test
     dtypes = ('float64', 'float32')
     bools = (True, False)
-    strides = (1,)  # TODO: Test random strides
+    strides = (1, None)
     uplos = ('l', 'u')
     trans_tuple = ('t', 'n')
     diags = ('n', 'u')
@@ -53,6 +53,8 @@ def test_trsv():
                          trans, "_",
                          diag)
             test_name = "".join(variables)
+
+            print(test_name)
             tests_failed.append(test_name)
 
     return tests_failed
@@ -68,7 +70,7 @@ def passed_test(dtype, as_matrix, b_is_row, stride, uplo, trans_a, diag):
         b_is_row:     True to test a row vector as parameter b, False to test a column vector
         stride:       stride of x and y to test; if None, a random stride is assigned
         uplo:         BLASpy uplo parameter to test
-        trans:        BLASpy trans parameter to test
+        trans_a:      BLASpy trans parameter to test
         diag:         BLASpy diag parameter to test
 
     Returns:
@@ -76,25 +78,36 @@ def passed_test(dtype, as_matrix, b_is_row, stride, uplo, trans_a, diag):
         False otherwise.
     """
 
-    b_is_row=False
-
     # generate random sizes for matrix/vector dimensions and vector stride (if necessary)
     n = randint(N_MIN, N_MAX)
-    #stride = randint(N_MIN, STRIDE_MAX) if stride is None else stride
+    stride = randint(N_MIN, STRIDE_MAX) if stride is None else stride
     n_A = n / stride + (n % stride > 0)
 
     # create random vectors and matrices to test
-    x = random_vector(n, False, dtype, as_matrix)
+    b = random_vector(n, b_is_row, dtype, as_matrix)
     A = random_triangular_matrix(n_A, dtype, as_matrix, uplo, diag)
+    A /= n_A  # scale off-diagonal to avoid numerical issues
+
+    # fill diagonal with 1 if unit-triangular, else fill diagonal with values between 1 and 2
     if diag == 'u':
         fill_diagonal(A, 1)
+    else:
+        for i in range(n_A):
+            A[i, i] = uniform(1, 2)
 
-    A_2 = A.T if trans_a == 't' or trans_a == 'T' else A
-    b = dot(A_2, x).T if b_is_row else dot(A_2, x)
-    b_2 = copy(b.T) if b_is_row else copy(b)
+    # copy b for comparison later
+    expected = copy(b.T) if b_is_row else copy(b)
 
+    # solve for x
     trsv(A, b, uplo, trans_a, diag, inc_b=stride)
     b = b.T if b_is_row else b
 
+    # compute actual result
+    actual = gemv(A, b, zeros((n, 1), dtype=dtype), trans_a=trans_a, inc_x=stride, inc_y=stride)
+    if stride != 1:
+        for i in range(n):
+            if i % stride != 0:  # zero out all non-relevant entries in expected vector
+                expected[i] = 0
+
     # compare the actual result to the expected result and return result of the test
-    return allclose(dot(A_2, b), b_2, RTOL, ATOL)
+    return allclose(actual, expected, RTOL, ATOL)
