079c9c23f09c2e1d9a2cd58119bd06117c9f7d90
nicholas-moreles <nicholas.moreles@utexas.edu>
2014-12-13 15:21:18 -0600

    Update config.py
    Details:
     - Minor changes to comments and imports

--- blaspy/config.py
+++ blaspy/config.py
@@ -8,11 +8,10 @@
     directory or at http://opensource.org/licenses/BSD-3-Clause
 
 """
-import os
 
 from .errors import raise_blas_os_error
-from ctypes import cdll, windll
-from os import path
+from ctypes import cdll
+from os import chdir, path
 from platform import system
 from struct import calcsize
 
@@ -25,12 +24,12 @@ BLAS_NAME_OVERRIDE = ""  # default is ""
 # False if Python should search for it.
 IN_BLASPY_SUBDIR = True  # default is True
 
-##################################
-# DO NOT EDIT BELOW THIS COMMENT #
-##################################
+###############################
+# DO NOT EDIT BELOW THIS LINE #
+###############################
 
 # find the appropriate BLAS to use
-if BLAS_NAME_OVERRIDE == "":
+if BLAS_NAME_OVERRIDE == "":  # try to use included OpenBLAS
     if system() == "Windows":
         if calcsize("P") == 8:  # 64-bit
             BLAS_NAME = "libopenblas-0.2.13-win64-int32.dll"
@@ -41,12 +40,12 @@ if BLAS_NAME_OVERRIDE == "":
             BLAS_NAME = "libopenblas-0.2.13-linux64.so"
         else:  # 32-bit
             BLAS_NAME = "libopenblas-0.2.10-linux32.so"
-    else:  # no appropriate BLAS implementation included, BLAS_NAME_OVERRIDE must be used
+    else:  # no appropriate OpenBLAS included, BLAS_NAME_OVERRIDE must be used
         raise_blas_os_error()
 else:
     BLAS_NAME = BLAS_NAME_OVERRIDE
 
 # Change the directory (required for Windows 64-bit) and load the library
 if IN_BLASPY_SUBDIR:
-    os.chdir(str(path.dirname(__file__))[:-6] + "lib")
+    chdir(str(path.dirname(__file__))[:-6] + "lib")
 _libblas = cdll.LoadLibrary(BLAS_NAME)
