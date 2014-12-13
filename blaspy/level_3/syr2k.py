37b17099f13733dcfe8dd1bc43e5596a7de6c748
nicholas-moreles <nicholas.moreles@utexas.edu>
2014-12-13 15:52:49 -0600

    Update syr2k
    Details:
     - Update documentation to new standards
     - Remove try block
     - Set default scalar values to 1.0 (previously 1)

--- blaspy/level_3/syr2k.py
+++ blaspy/level_3/syr2k.py
@@ -12,11 +12,10 @@
 from ..helpers import (get_matrix_dimensions, get_square_matrix_dimension, create_zero_matrix,
                        check_equal_sizes, convert_uplo, convert_trans, get_cblas_info, ROW_MAJOR,
                        TRANS)
-from ..errors import raise_generic_type_error
 from ctypes import c_int, POINTER
 
 
-def syr2k(A, B, C=None, uplo='u', trans='n', alpha=1, beta=1, lda=None, ldb=None, ldc=None):
+def syr2k(A, B, C=None, uplo='u', trans='n', alpha=1.0, beta=1.0, lda=None, ldb=None, ldc=None):
     """
     Perform a symmetric rank-2k update operation.
 
@@ -35,87 +34,90 @@ def syr2k(A, B, C=None, uplo='u', trans='n', alpha=1, beta=1, lda=None, ldb=None
     newly created matrix C.
 
     Args:
-        A:       2D numpy matrix or ndarray representing matrix A
-        B:       2D numpy matrix or ndarray representing matrix B
+        A:        2D NumPy matrix or ndarray representing matrix A
+        B:        2D NumPy matrix or ndarray representing matrix B
 
         --optional arguments--
 
-        C:       2D numpy matrix or ndarray representing matrix C (default is zero matrix)
-        uplo:    'u'  if the upper triangular part of C is to be used/overwritten
-                 'l'  if the lower triangular part of C is to be used/overwritten
-        trans:   'n'  if the operation is to proceed as if A and B are not transposed
-                 't'  if the operation is to proceed as if A and B are transposed
-        alpha:   scalar alpha
-        beta:    scalar beta
-        lda:     leading dimension of A (must be >= # of cols in A)
-        ldb:     leading dimension of B (must be >= # of cols in B)
-        ldc:     leading dimension of C (must be >= # of cols in C)
+        C:        2D NumPy matrix or ndarray representing matrix C
+                      < default is the zero matrix >
+        uplo:     'u'  if the upper triangular part of C is to be used/overwritten
+                  'l'  if the lower triangular part of C is to be used/overwritten
+                      < default is 'u' >
+        trans:    'n'  if the operation is to proceed as if A and B are not transposed
+                  't'  if the operation is to proceed as if A and B are transposed
+                      < default is 'n' >
+        alpha:    scalar alpha
+                      < default is 1.0 >
+        beta:     scalar beta
+                      < default is 1.0 >
+        lda:      leading dimension of A (must be >= # of cols in A)
+                      < default is the number of columns in A >
+        ldb:      leading dimension of B (must be >= # of cols in B)
+                      < default is the number of columns in B >
+        ldc:      leading dimension of C (must be >= # of cols in C)
+                      < default is the number of columns in C >
 
     Returns:
         matrix C (which is also overwritten)
 
     Raises:
-        TypeError:  if A, B, or C is not a 2D NumPy ndarray or NumPy matrix
-
         ValueError: if any of the following conditions occur:
+                    - A, B, or C is not a 2D NumPy ndarray or NumPy matrix
                     - A, B, and C do not have the same dtype or that dtype is not supported
                     - C is not a square matrix
-                    - The dimensions of A, B and C do not conform
+                    - the dimensions of A, B and C do not conform
                     - 'uplo' is not equal to one of the following: 'u', 'U', 'l', 'L'
                     - 'trans' is not equal to one of the following: 'n', 'N', 't', 'T'
     """
 
-    try:
-        # convert to appropriate CBLAS value
-        cblas_uplo = convert_uplo(uplo)
-        cblas_trans = convert_trans(trans)
-        transpose = cblas_trans == TRANS
+    # convert to appropriate CBLAS value
+    cblas_uplo = convert_uplo(uplo)
+    cblas_trans = convert_trans(trans)
+    transpose = cblas_trans == TRANS
 
-        # get the dimensions of the parameters
-        m_A, n_A = get_matrix_dimensions('A', A)
-        m_B, n_B = get_matrix_dimensions('B', B)
-        n, k = (m_A, n_A) if not transpose else (n_A, m_A)
+    # get the dimensions of the parameters
+    m_A, n_A = get_matrix_dimensions('A', A)
+    m_B, n_B = get_matrix_dimensions('B', B)
+    n, k = (m_A, n_A) if not transpose else (n_A, m_A)
 
-        # ensure the matrix dimensions conform for the desired operation
-        check_equal_sizes('A', m_A, 'B', m_B) # both m and n must match for A and B regardless
-        check_equal_sizes('A', n_A, 'B', n_B) # of whether the matrices are being transposed
+    # ensure the matrix dimensions conform for the desired operation
+    check_equal_sizes('A', m_A, 'B', m_B) # both m and n must match for A and B regardless
+    check_equal_sizes('A', n_A, 'B', n_B) # of whether the matrices are being transposed
 
-        # if C is not given, create zero matrix with same type as A
-        if C is None:
-            C = create_zero_matrix(n, n, A.dtype, type(A))
-            ldc = None
+    # if C is not given, create zero matrix with same type as A
+    if C is None:
+        C = create_zero_matrix(n, n, A.dtype, type(A))
+        ldc = None
 
-        # continue getting dimensions of the parameters
-        dim_C = get_square_matrix_dimension('C', C)
+    # continue getting dimensions of the parameters
+    dim_C = get_square_matrix_dimension('C', C)
 
-        # assign a default value to lda, ldb, and ldc if necessary (assumes row-major order)
-        if lda is None:
-            lda = n_A
-        if ldb is None:
-            ldb = n_B
-        if ldc is None:
-            ldc = dim_C
+    # assign a default value to lda, ldb, and ldc if necessary (assumes row-major order)
+    if lda is None:
+        lda = n_A
+    if ldb is None:
+        ldb = n_B
+    if ldc is None:
+        ldc = dim_C
 
-        # ensure the matrix dimensions conform for the desired operation
-        check_equal_sizes('A', n, 'C', dim_C)
+    # ensure the matrix dimensions conform for the desired operation
+    check_equal_sizes('A', n, 'C', dim_C)
 
-        # determine which CBLAS subroutine to call and which ctypes data type to use
-        cblas_func, ctype_dtype = get_cblas_info('syr2k', (A.dtype, B.dtype, C.dtype))
+    # determine which CBLAS subroutine to call and which ctypes data type to use
+    cblas_func, ctype_dtype = get_cblas_info('syr2k', (A.dtype, B.dtype, C.dtype))
 
-        # create a ctypes POINTER for each matrix
-        ctype_A = POINTER(ctype_dtype * n_A * m_A)
-        ctype_B = POINTER(ctype_dtype * n_B * m_B)
-        ctype_C = POINTER(ctype_dtype * dim_C * dim_C)
+    # create a ctypes POINTER for each matrix
+    ctype_A = POINTER(ctype_dtype * n_A * m_A)
+    ctype_B = POINTER(ctype_dtype * n_B * m_B)
+    ctype_C = POINTER(ctype_dtype * dim_C * dim_C)
 
-        # call CBLAS using ctypes
-        cblas_func.argtypes = [c_int, c_int, c_int, c_int, c_int, ctype_dtype,
-                               ctype_A, c_int, ctype_B, c_int, ctype_dtype, ctype_C, c_int]
-        cblas_func.restype = None
-        cblas_func(ROW_MAJOR, cblas_uplo, cblas_trans, n, k, alpha,
-                   A.ctypes.data_as(ctype_A), lda, B.ctypes.data_as(ctype_B), ldb, beta,
-                   C.ctypes.data_as(ctype_C), ldc)
+    # call CBLAS using ctypes
+    cblas_func.argtypes = [c_int, c_int, c_int, c_int, c_int, ctype_dtype,
+                           ctype_A, c_int, ctype_B, c_int, ctype_dtype, ctype_C, c_int]
+    cblas_func.restype = None
+    cblas_func(ROW_MAJOR, cblas_uplo, cblas_trans, n, k, alpha,
+               A.ctypes.data_as(ctype_A), lda, B.ctypes.data_as(ctype_B), ldb, beta,
+               C.ctypes.data_as(ctype_C), ldc)
 
-        return C  # C is also overwritten
-
-    except (AttributeError, TypeError):
-        raise_generic_type_error()
+    return C  # C is also overwritten
