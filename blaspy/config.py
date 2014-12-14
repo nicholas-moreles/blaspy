0d4c7269b9c3fc50b910d234476500713124b919
nicholas-moreles <nicholas.moreles@utexas.edu>
2014-12-14 15:01:14 -0600

    Update included OpenBLAS
    Details:
     - Rearrange directory structure of lib folder
     - Include extra .dll files for 32-bit Windows
     - Update config.py

--- blaspy/config.py
+++ blaspy/config.py
@@ -18,34 +18,38 @@ from struct import calcsize
 # The name of the BLAS .so or .dll file. By default this is the OpenBLAS reference
 # implementation bundled with BLASpy. Only modify if you wish to use a different version of BLAS
 # or if your operating system is not supported by BLASpy out of the box.
-BLAS_NAME_OVERRIDE = ""  # default is ""
+BLAS_NAME = ""  # default is ""
 
 # True if the BLAS .so or .dll file is in the blaspy/lib subdirectory,
 # False if Python should search for it.
-IN_BLASPY_SUBDIR = True  # default is True
+IN_BLASPY_SUBDIRECTORY = True  # default is True
 
 ###############################
 # DO NOT EDIT BELOW THIS LINE #
 ###############################
 
 # find the appropriate BLAS to use
-if BLAS_NAME_OVERRIDE == "":  # try to use included OpenBLAS
+if BLAS_NAME == "":  # try to use included OpenBLAS
     if system() == "Windows":
         if calcsize("P") == 8:  # 64-bit
             BLAS_NAME = "libopenblas-0.2.13-win64-int32.dll"
+            SUB_DIRECTORY = "win64"
         else:  # 32-bit
             BLAS_NAME = "libopenblas-0.2.13-win32.dll"
+            SUB_DIRECTORY = "win32"
     elif system() == "Linux":
         if calcsize("P") == 8:  # 64-bit
             BLAS_NAME = "libopenblas-0.2.13-linux64.so"
+            SUB_DIRECTORY = "linux64"
         else:  # 32-bit
-            BLAS_NAME = "libopenblas-0.2.10-linux32.so"
+            BLAS_NAME = "libopenblas-0.2.13-linux32.so"
+            SUB_DIRECTORY = "linux32"
     else:  # no appropriate OpenBLAS included, BLAS_NAME_OVERRIDE must be used
         raise_blas_os_error()
 else:
-    BLAS_NAME = BLAS_NAME_OVERRIDE
+    SUB_DIRECTORY = ""
 
-# Change the directory (required for Windows 64-bit) and load the library
-if IN_BLASPY_SUBDIR:
-    chdir(str(path.dirname(__file__))[:-6] + "lib")
+# Change the directory and load the library
+if IN_BLASPY_SUBDIRECTORY:
+    chdir(str(path.dirname(__file__))[:-6] + "lib/" + SUB_DIRECTORY)
 _libblas = cdll.LoadLibrary(BLAS_NAME)
